913c527fe6ca56ba48b18b75c0ad5fa9ff5878d4
Valeriy <totubalinvaleriy@mail.ru>
2020-07-15 13:45:36 +0300

    авторизация

--- realty/routes/web.php
+++ realty/routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::prefix('admin')->get('/', 'AdminController@handle');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
